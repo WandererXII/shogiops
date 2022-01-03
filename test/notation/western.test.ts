94d9e85a27eb63c442aa30e3148c87ce4946a36e
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-01-03 19:29:21 +0100

    Add move notation test for minishogi

--- test/notation/western.test.ts
+++ test/notation/western.test.ts
@@ -1,6 +1,7 @@
 import { parseUsi } from '../../src';
 import { Shogi } from '../../src/shogi';
 import { makeWesternMove } from '../../src/notation/western';
+import { Minishogi } from '../../src/variant';
 
 test('basic moves', () => {
   const pos = Shogi.default();
@@ -14,4 +15,10 @@ test('amb moves', () => {
   const pos = Shogi.default();
   const move = parseUsi('6i5h')!;
   expect(makeWesternMove(pos, move)).toEqual('G69-58');
+});
+
+test('minishogi move', () => {
+  const pos = Minishogi.default();
+  const move = parseUsi('4e4d')!;
+  expect(makeWesternMove(pos, move)).toEqual('G-44');
 });

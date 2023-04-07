b108534a30b8d4c4164d94394d52be69161aff8d
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-04-07 17:24:01 +0200

    Add more tests

--- test/variant/annan.test.ts
+++ test/variant/annan.test.ts
@@ -9,6 +9,10 @@ import { perfts } from '../fixtures/perftAnnan';
 const annanPerfts: [string, number, number][] = [
   ['', 1, 28],
   ['', 2, 784],
+  ['4k3S/r1l1gs3/n+N1s5/L+P4G1+B/1Pp1p1Rb1/4PNpN1/3+pPGPP1/6G1P/1p1K2S1+l w - 1', 1, 35],
+  ['P1G+N3s1/lp7/2+N+Pp+P1+L+P/1sB1kP3/N1pl1s3/g6p1/+p1B2+p+p+l+p/1+s+r+n4R/1g1pK1+p2 b G3P 167', 1, 2],
+  ['1n2g1g2/l2k1p3/p1p1psb1l/r4Ppp1/1p6p/P2p1PPpP/L3+n1S1L/1BKN2GS1/r1G4N1 b 2Psp 83', 1, 7],
+  ['Pn2g4/+S2ps3l/4p1p1+P/sPpg1Pk2/lS1rr2pp/p2GnnPP1/LB+p4+nL/2B2K2+p/1p1GP2+p1 b - 129', 1, 46],
 ];
 
 test.each(annanPerfts)('annan perft: %s (%s): %s', (sfen, depth, res) => {
@@ -110,7 +114,9 @@ test('promotions', () => {
 test('capture attacker move giver', () => {
   const pos = parseSfen('annan', '5k3/9/9/7b1/5P3/5L3/9/9/5K3 w - 1').unwrap();
   expect(pos.moveDests(parseSquareName('2d')).has(parseSquareName('4f'))).toBe(true);
-
+  const posS = parseSfen('annan', '5k3/9/9/5l3/5p3/9/2B6/9/5K3 b - 1').unwrap();
+  expect(posS.isLegal(parseUsi('7g4d')!)).toBe(true);
+  expect(perft(posS, 1, false)).toBe(5);
   // 2 checkers
   const pos2 = parseSfen(
     'annan',
@@ -137,6 +143,19 @@ test('capture attacker move giver', () => {
 test('capture attacker move giver - with king', () => {
   const pos = parseSfen('annan', '9/7Pk/7+R1/9/9/9/9/9/8K w - 1').unwrap();
   expect(pos.moveDests(parseSquareName('1b')).has(parseSquareName('2c'))).toBe(true);
+
+  const pos2 = parseSfen('annan', '9/8k/9/9/9/9/9/p+r7/K8 b - 1').unwrap();
+  expect(pos2.moveDests(parseSquareName('9i')).has(parseSquareName('8h'))).toBe(true);
+});
+
+test('do not allow discovering check by capturing move giver', () => {
+  const pos = parseSfen('annan', '3k5/9/9/3p5/3bG4/9/9/6K2/9 b').unwrap();
+  expect(pos.moveDests(parseSquareName('5e')).has(parseSquareName('6d'))).toBe(false);
+  expect(perft(pos, 1, false)).toBe(13);
+
+  const pos2 = parseSfen('annan', '6k2/p8/9/3Bs4/3R5/9/9/6K2/9 w').unwrap();
+  expect(pos2.moveDests(parseSquareName('5d')).has(parseSquareName('6e'))).toBe(false);
+  expect(perft(pos2, 1, false)).toBe(10);
 });
 
 test('proper parse', () => {

db3542b4098ab57752968588191c6b2059338bea
WandererXII <43043181+WandererXII@users.noreply.github.com>
2026-01-09 16:17:12 +0100

    Now not needed types for vitest

--- test/vitest.d.ts
+++
@@ -1,9 +0,0 @@
-// https://github.com/vitejs/vite/issues/9813
-/** biome-ignore-all lint/complexity/noBannedTypes: vitest... */
-
-declare type Worker = {};
-declare type WebSocket = {};
-
-declare namespace WebAssembly {
-  type Module = {};
-}
